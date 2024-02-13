ebbf37b99bad819065cf9197ef15fc2504514b00
Alex <157164614+nxtalex@users.noreply.github.com>
2024-02-13 22:47:50 +0100

    Update contact.js

--- app/models/contact.js
+++ app/models/contact.js
@@ -9,12 +9,6 @@ const contactSchema = new Schema({
     maxLength: [50, "Name must be lesser than 50 characters"],
   },
 
-  email: {
-    type: String,
-    required: [true, "Email is required."],
-    match: [/^[\w.%+-]+@[\w.-]+\.[A-Za-z]{2,}$/i, "Invalid email address"],
-  },
-
   message: {
     type: String,
     required: [true, "Message is required."],
